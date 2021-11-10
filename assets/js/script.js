d91bc0242ab0b2e8d510177c099e9d4d85ec41de
Mona Lisa <mekotigi@gmail.com>
2021-11-09 21:45:08 -0500

    change this if statment change

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,21 @@
 
 //declaring variables for value uppercase lowercase numbers and symbols
-var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var lowercase = "abcdefghijklmnopqrstuvwxyz";
-var number = "0123456789";
-var symbol = "!@#$%^&*()_+=-}{[]?/>.<,':;`~/\|";
-var value  =[] ;
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";        //ASCHII value 65- 90
+var lowercase = "abcdefghijklmnopqrstuvwxyz";       // ASCHII value 97= 122
+var number = "0123456789";                       //ASCHII value 48 -57
+var symbol = "!@#$%^&*()_+=-}{[]?/>.<,':;`~/\|";           //ASCHII value 33-47 , 59-64,
+var value  = [uppercase] + [lowercase] + [symbol]+ [number] ;
 var uppercaseEl = document.getElementById("uppercase");
 var lowercaseEl = document.getElementById("lowercase");
 var numberEl = document.getElementById("number");
 var symbolEl = document.getElementById("symbol");
 
- 
-
-if ( uppercaseEl.checked)
+if (( !uppercaseEl.checked ) && ( !lowercaseEl.checked ) && 
+( !numberEl.checked ) &&  ( !symbolEl.checked ))
+{
+  value = [uppercase] + [lowercase] + [symbol]+ [number] ;
+}
+else if ( uppercaseEl.checked)
 {
   value = [lowercase]  + [number] + [symbol];
 }
@@ -30,7 +33,7 @@ else if ( symbolEl.checked)
 }
 else
 {
-  value = [uppercase] + [lowercase] + [symbol]+ [number] ;
+  value = 0 ;
 }
 
 
@@ -54,17 +57,17 @@ var password = "";
  
   
  // // Add event listener to generate button
-document.getElementById("display").value = password;
+document.getElementById("display" ).value = password ; 
 DocumentFragment.execCommand;
 
 //add pasword to previously generated passwords section
-document.getElementById("lastNums" ).innerHTML += password ;
+document.getElementById("lastNums" ).innerHTML += password + "</br>" ;
 
 
 }
 
 // //default length to display
- document.getElementById( "length" ).innerHTML = "length: 124";
+ document.getElementById( "length" ).innerHTML = "length: 124" ;
 
 // //function to set length based on display position
 document.getElementById( "numberlength" ).oninput = function ()
@@ -73,7 +76,7 @@ document.getElementById( "numberlength" ).oninput = function ()
     if ( document.getElementById( "numberlength" ).value >0 )
     {
  
-        document.getElementById( 'length'  ).innerHTML ="length:"  +  document.getElementById("numberlength" ).value + "\n";  
+        document.getElementById( 'length'  ).innerHTML ="length:"  +  document.getElementById("numberlength" ).value ;  
         
     }
     else{
